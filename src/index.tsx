8af938c7aeabe4902b7e5d68714978d6bcc7f121
jeovanedossantossantos <jeovane.ssantos@outlook.com>
2022-10-29 10:09:23 -0300

    chore: screen orientation setting

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import { registerRootComponent } from 'expo';
-
+import * as ScreenOrientation from 'expo-screen-orientation';
 import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
+async function changeScreenOrientation() {
+    await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
+}
+
+changeScreenOrientation()
 registerRootComponent(App);
